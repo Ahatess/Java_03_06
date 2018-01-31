78945dc33c11fa280d0c4477406d0f132c9639be
Andrius <andriuscere@gmail.com>
2018-01-31 16:16:12 +0200

    Uzduotis0306

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private String[] mas = new String[5];
+
     public static void main(String[] args) {
-	// write your code here
+        Main objektas = new Main();
+        objektas.masyvas();
+        objektas.rasymas();
+    }
+
+    public void masyvas() {
+        Scanner sc = new Scanner(System.in);
+        mas = new String[5];
+        for (int i = 0; i < mas.length; i++) {
+
+            System.out.println("Iveskite zodi");
+            mas[i] = sc.nextLine();
+        }
+    }
+
+    public void rasymas() {
+        for (int i = 0; i < mas.length; i++) {
+            System.out.println("Masyvo " + i + "zodis yra" + mas[i]);
+        }
+
     }
 }
